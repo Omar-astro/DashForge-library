5228b9cbed20fae609ae8ec721cd448918d47a2b
Omar-astro <omar.ashraf.hamed2017@gmail.com>
2026-07-31 17:07:59 +0300

    Release v1.0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+Project links: [GitHub](https://github.com/Omar-astro/DashForge-library) · [Documentation](https://omar-astro.github.io/DashForge-library/) · [PyPI](https://pypi.org/project/dashforge/)
+
+## 1.0.3
+- Updated package, PyPI, and documentation links.
+- Updated examples to import DashForge from the published package.
+
 ## 1.0.2
 - Fixed PyPI-safe package description.
 
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ description = Path("README_PYPI.md").read_text(encoding="utf-8")
 
 setup(
     name='dashforge',
-    version='1.0.2',
+    version='1.0.3',
     packages=find_packages(),
     install_requires=[
         "dash>=3.3.0",
@@ -18,7 +18,8 @@ setup(
     long_description=description,
     long_description_content_type="text/markdown",
     project_urls={
-        "GitHub": "",
-        "Changelog": "",
+        "GitHub": "https://github.com/Omar-astro/DashForge-library",
+        "Changelog": "https://github.com/Omar-astro/DashForge-library/blob/main/CHANGELOG.md",
+        "Documentation": "https://omar-astro.github.io/DashForge-library/",
     },
 )
