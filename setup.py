80ae397e382ec2b213f2d0f0c55d12817726ea5e
Omar-astro <omar.ashraf.hamed2017@gmail.com>
2026-08-01 12:36:21 +0300

    Updated Setup.py

--- setup.py
+++ setup.py
@@ -6,20 +6,47 @@ description = Path("README_PYPI.md").read_text(encoding="utf-8")
 
 setup(
     name='dashforge',
-    version='1.0.3',
+    version='1.0.4',
+    author="Omar Ashraf",
+    author_email="omar.ashraf.hamed2017@gmail.com",
+    description="Turn Plotly charts into interactive web dashboards with just a few lines of Python.",
+
     packages=find_packages(),
     install_requires=[
         "dash>=3.3.0",
-        "Flask>=2.3.0",
+        "flask>=2.3.0",
         "pandas>=2.3.3",
         "param>=2.4.1",
         "plotly>=6.4.0",
     ],
     long_description=description,
     long_description_content_type="text/markdown",
+
+    url="https://github.com/Omar-astro/DashForge-library",
     project_urls={
+        "Documentation": "https://omar-astro.github.io/DashForge-library/",
         "GitHub": "https://github.com/Omar-astro/DashForge-library",
         "Changelog": "https://github.com/Omar-astro/DashForge-library/blob/main/CHANGELOG.md",
-        "Documentation": "https://omar-astro.github.io/DashForge-library/",
+        "Issues": "https://github.com/Omar-astro/DashForge-library/issues",
     },
+
+    license="MIT",
+
+    keywords=[
+        "dashboard",
+        "plotly",
+        "dash",
+        "visualization",
+        "data-analysis",
+        "analytics",
+        "python",
+    ],
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Topic :: Software Development :: Libraries",
+    ],
 )
